288ecdc1dab15cae3e1751fa7caf6aff13b4b4b2
unknown <imamovic.kerim@gmail.comgit config --global user.name kimamovic21>
2022-02-11 16:36:03 +0100

    deploy my app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Values from 'values.js';
+import SingleColor from './SingleColor';
 
-function App() {
+function App() {  
+  
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+  const [list, setList] = useState(new Values("#ff7777").all(10));
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(10);
+      setList(colors);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <section className='container'>
+            <h3>Colour generator</h3>
+            <form onSubmit={handleSubmit}>
+              <input type='text' 
+                     placeholder='ff7777'
+                     value={color}
+                     onChange={(e) => setColor(e.target.value)}
+                     className={`${error ? 'error' : null}`}
+              />
+              <button className='btn' type='submit'>
+                Get colours
+              </button>
+            </form>
+        </section>
+
+        <section className='colors'>
+          {list.map((color, index) => {
+            return (
+              <SingleColor key={index} 
+                          {...color} 
+                          {...index}
+                          index={index}
+                          hexColor={color.hex}
+               />
+            );
+          })}
+        </section>
+    </>
   );
 }
 
